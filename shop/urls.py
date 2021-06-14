d4b29a01f57c844a79fda478179ffdae92045242
Sejal Jain <sejal08082000@gmail.com>
2021-06-14 23:14:49 +0530

    edit

--- shop/urls.py
+++ shop/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from.import views
+from . import views
 
 urlpatterns = [
-    path("", views.index,name="ShopHome"),
-    path("about/", views.about,name="aboutus"),
-    path("contact/", views.contact,name="contactus"),
-    path("search/", views.search,name="search"),
-    path("tracker/", views.tracker,name="track"),
-    path("Product/", views.product,name="product"),
-    path("checkout/", views.checkout,name="checkout"),
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="aboutus"),
+    path("contact/", views.contact, name="contactus"),
+    path("search/", views.search, name="search"),
+    path("tracker/", views.tracker, name="track"),
+    path("Product/", views.product, name="product"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("meeting/", views.meeting, name="meeting"),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,31 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, contct
 from math import ceil
+
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nslides = n//4 + ceil((n/4)-(n//4))
-    params = {'product': products}
-    return render(request,'shop/index.html',params)
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nslides = n//4 + ceil((n/4)-(n//4))
+    # params = {'product': products}
+    # return render(request,'shop/index.html',params)
+    return render(request, 'shop/index.html')
+
 
 def about(request):
-    return render(request,'shop/about.html')
+    return render(request, 'shop/about.html')
+
 
 def contact(request):
-    return render(request,'shop/contact.html')
+    return render(request, 'shop/contact.html')
+
 
 def search(request):
-    return  HttpResponse("we're at search")
+    return HttpResponse("we're at search")
+
 
 def tracker(request):
-    return  HttpResponse("we're at tracker")
+    return HttpResponse("we're at tracker")
+
 
 def product(request):
-    return  HttpResponse("we're at product view")
+    return HttpResponse("we're at product view")
+
 
 def checkout(request):
-    return  HttpResponse("we're at checkout")
+    return HttpResponse("we're at checkout")
+
+
+def meeting(request):
+    if request.method == "POST":
+        print(request)
+        name = request.POST.get('name', '')
+        phone = request.POST.get('contact', '')
+        email = request.POST.get('email', '')
+        desc = request.POST.get('anything', '')
+        check1 = request.POST.get('check1', 'off')
+        check2 = request.POST.get('check2', 'off')
+        meeting= contct(name=name, phone=phone, email=email, desc=desc, check1=check1, check2=check2)
+        meeting.save()
+
+    return render(request, 'shop/index.html')
